07d760b8a81092564c94c9da4fc9905334f99683
Denys Palianytsia <denpalyan@gmail.com>
2023-03-14 22:30:45 +0200

    api tasks

--- weather.py
+++ weather.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 
-city = "Kyiv"
+city = str(input("Введіть назву міста: "))
 api_key = "e4802e1ebecfffb4ed53e0c63027be2e"
 url = "http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 result = requests.get(url)
 
-print(json.loads(result.text))
+print(result.text)
